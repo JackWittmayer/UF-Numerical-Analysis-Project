00010d8680d110b6331b88f2c10f77d3bf375d0b
hutchinsonvandyke <hutchinsonrvandy@ufl.edu>
2020-11-13 16:31:32 -0500

    hehehe

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 import random
 from PIL import Image
 from dataLoader import loadData
-from sklearn import metrics
+from sklearn.cluster import MiniBatchKMeans
 
 ''' k means algorithm steps:
 1. pick random representatives
@@ -38,13 +38,14 @@ def findClassAvg(rep, images):
     # Create empty list of pixels:
     avgPix = [0] * len(images[0]['pix'])
     classImageCount = 0
+    count = 0
     for image in images:
 
         # if the image belongs to the class we are looking for:
         if image['class'] == rep['class']:
-
             # Skip images that are missing pixels:
             if image['pix'] == None:
+                count += 1
                 continue
 
             # tally up the number of images that we are averaging:
@@ -52,12 +53,24 @@ def findClassAvg(rep, images):
 
             # iterate over all pixels in the image and add it to the respective pixel of avgPix:
             for i in range(len(image['pix'])):
-                avgPix[i] += image['pix'][i]
-
+                avgPix[i] += float(image['pix'][i])
+    
     # Divide each sum of pixels by the number of images to create an average:
     for i in range(len(avgPix)):
-        avgPix[i] //= classImageCount
+        if classImageCount == 0:
+            print('something is fucked!')
+        avgPix[i] /= classImageCount
     return avgPix
+
+#function to track convergence of algorithm
+def Jclust(reps, images):
+    sum = 0
+    for rep in reps:
+        for image in images:
+            if rep['class'] == image['class']:
+                for j in range(len(image['pix'])):
+                    sum = sum + abs(image['pix'][j] - rep['pix'][j])**2
+    return (sum/len(images))
 
 # Experimental function to average all the faces for a given ethnicity (takes forever probably doesn't work)
 def findAvgFace(images, ethnicities, selection):
@@ -83,36 +96,46 @@ def findAvgFace(images, ethnicities, selection):
 
 
 # Attempted implementation of the kmeans algorithm the professor showed in class:
-def kmeans(images):
+def kmeans(imageInput, trainingSetSize, inputReps):
     clusterNumber = 4 # number of expected clusters, will probably be in the order of hundreds
-    iterationCount = 15 # number of times to create representatives, find distance to reps, etc.
-
+    #iterations count should be dependent on convergence of Jclust function
+    images = []
+    trainingSetIndicies = []
     # pick k random images from images, using all of them takes forever:
-    images = random.choices(images, k = 100)
+    #images = random.choices(images, k = 1000)
 
-    # turn list of space separated pixel values into list of lists of pixel ints with class label:
-    # TODO: Move this code into dataLoader so we can start off with the proper format
-    for j in range(len(images)):
-        # Turn space separated string of numbers into list of number strings:
-        images[j] = images[j].split() # "123 234 212" -> ['123, '234', '212']
-
-        # Turn every string number into an integer:
-        for i in range(len(images[j])):
-            images[j][i] = int(images[j][i])
-
-        # Turn list of lists of pixels into a list of dictionaries containing entries for
-        # a list of pixels ('pix) and a class value ('class'):
-        images[j] = {'pix': images[j], 'class': 0}
+    ##Data selection, tracking, then normalization
+    
+    set = list(range(0, 100))
+    #set = list(range(0, len(imageInput)-1))
+    random.shuffle(set)
+    for i in range(trainingSetSize):
+        images.append(imageInput[set[i]])
+        trainingSetIndicies.append(set[i])
+    
+    for image in images:
+        for j in range(len(image['pix'])):
+            image['pix'][j] = image['pix'][j] / 255.0
+        
+   
+    
 
     # PART 1: CREATE [clusterNumber] RANDOM REPRESENTATIVES:
     reps = []
-    for i in range(clusterNumber):
-        # Grab a random image from images, copy its pixels, and create a new dictionary from it:
-        rep = {'pix': images[random.randint(0, len(images)-1)]['pix'].copy(), 'class': i}
-        reps.append(rep)
+    JClusterResults = []
+    if len(inputReps) == 0:
+        for i in range(clusterNumber):
+            # Grab a random image from images, copy its pixels, and create a new dictionary from it:
+            rep = {'pix': images[random.randint(0, len(images)-1)]['pix'].copy(), 'class': i}
+            reps.append(rep)
+    else:
+        reps = inputReps
 
     # Do the next two parts [iterationCount] times:
-    for i in range(iterationCount):
+    loop = 0
+    converged = False
+    while (converged == False):
+        loop += 1
         # PART 2: ASSIGN CLASS TO EACH DATA POINT (IMAGE) BASED ON CLOSEST REPRESENTATIVE:
         # Find minimum distance from an image to a representative:
         for i in range(len(images)):
@@ -129,37 +152,87 @@ def kmeans(images):
                     images[i]['class'] = rep['class']
 
         # PART 3: FIND AVERAGE POSITION OF EACH IMAGE IN EACH CLASS, MAKE THAT THE NEW REPRESENTATIVE:
+        
         for i in range(len(reps)):
             # calculate average:
             reps[i]['pix'] = findClassAvg(reps[i], images)
-
+        JClusterResults.append(Jclust(reps, images))
+        #if a couple of iterations have gone by and the Jclust function isn't decreasing by more than .5% we have converged
+        if (len(JClusterResults) > 4):
+            if (JClusterResults[len(JClusterResults)-2] / JClusterResults[len(JClusterResults)-2] > .999):
+                converged = True 
+    
     # Return the reps so we can see how they were changed:
-    return reps
+    return reps, JClusterResults, images, trainingSetIndicies
 
+def accuracy_test(images, ethnicity, trainingSet):
+    
+    count = 0
+    #re-normalizing!
+    for image in images:
+        for j in range((len(image['pix']))):
+            image['pix'][j] = image['pix'][j] * 255.0
+
+    for i in range(len(trainingSet)):
+        if i < 5: 
+            displayImage(images[i]['pix'])
+            displayImage(imageData[trainingSet[i]]['pix'])
+        if int(images[i]['class']) == int(ethnicity[trainingSet[i]]):
+            count += 1
+            
+    return count/len(trainingSet)
+
+def predetermineReps(imageData, inputReps):
+    for i in range(5):
+        set_ = [356, 367 ,91 , 258, 197]
+        rep = {'pix': imageData[set_[i]]['pix'].copy(), 'class': i}
+        inputReps.append(rep)
+    for rep in inputReps:
+        for j in range((len(rep['pix']))):
+            rep['pix'][j] = rep['pix'][j] / 255.0 
+    return inputReps
 
 rows = []
 pixels = []
+imageData = []
 labels = []
 ethnicity = []
-rows, pixels, labels, ethnicity = loadData()
+rows, imageData, labels, ethnicity, pixels = loadData()
+inputReps = []
+inputRepClasses = []
 
-reps = kmeans(pixels)
-for rep in reps:
-    displayImage(rep['pix'])
+inputReps = predetermineReps(imageData, inputReps)
+
+
+
+reps, JClustResults, imageResults, trainingIndices  = kmeans(imageData, 100, inputReps)
+print(accuracy_test(imageResults, ethnicity, trainingIndices))
+
+print(JClustResults)
+print(len(JClustResults))
+
+
+""" for rep in reps:
+    
+    for i in range(len(rep['pix'])):
+        rep['pix'][i] = rep['pix'][i] * 255
+    displayImage(rep['pix']) """
+
+
+
 #rows = np.array(rows, dtype=object)
 #pixels = np.array(pixels, dtype=object)
-dataSize = (len(rows))
+""" dataSize = (len(rows))
 trainingSetSize = 19754
 testDataSize = dataSize - 19754
 x_train = []
 x_test = []
 y_train = []
 y_test = []
-print(len(pixels))
 
-luckyHat = random.sample(range(23705), 19754)
+luckyHat = random.sample(range(23705), 1000)
 luckyHat.sort(reverse=True)
-for i in range(0, 19754):
+for i in range(0, len(luckyHat)):
     x_train.append(pixels[luckyHat[i]])
     y_train.append(ethnicity[luckyHat[i]])
     del pixels[luckyHat[i]]
@@ -168,14 +241,22 @@ for i in range(0, 19754):
 for i in range(0, testDataSize):
     x_test.append(pixels[i])
     y_test.append(ethnicity[i])
-x_train = np.array(x_train, dtype=object)
-x_test = np.array(x_test, dtype=object)
-y_train = np.array(y_train, dtype=object)
-y_test = np.array(y_test, dtype=object)
+x_train = np.array(x_train, dtype=np.uint8)
+x_test = np.array(x_test, dtype=np.uint8)
+x_train = int(x_train / 255.0)
+x_test = int(x_test / 255.0)
+y_train = np.array(y_train)
+y_test = np.array(y_test)
 print(x_train.shape)
+ 
+kMeansAlg = MiniBatchKMeans(n_clusters=4).fit(x_train)
+predictions = kMeansAlg.predict(x_test)
 
-##so x train is 19754 ID array but in medium tutorial it is (19754, 2304)
-#2304 cuz we got 48x48 images...will fix later
+for i in range(len(predictions)):
+    count = 0
+    if int(predictions[i]) == int(y_test[i]):
+        count += 1
+print('accuracy of library func: ' + str(count/len(predictions))) """
 
 
 
