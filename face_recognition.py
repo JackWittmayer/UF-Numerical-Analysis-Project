5966e67c9f93d6d65ba297a6c94ab45c9f7627d6
Jack <jwittmayer@ufl.edu>
2020-12-24 15:11:38 -0500

    removed excessive comments face_recognition.py

--- face_recognition.py
+++ face_recognition.py
@@ -30,10 +30,6 @@ def processOneImage(filename):
 
     win.clear_overlay()
     win.set_image(img)
-
-    # Ask the detector to find the bounding boxes of each face. The 1 in the
-    # second argument indicates that we should upsample the image 1 time. This
-    # will make everything bigger and allow us to detect more faces.
     dets = detector(img, 1)
     #print("Number of faces detected: {}".format(len(dets)))
 
@@ -47,12 +43,6 @@ def processOneImage(filename):
         win.clear_overlay()
         win.add_overlay(d)
         win.add_overlay(shape)
-
-        # Compute the 128D vector that describes the face in img identified by
-        # shape.  In general, if two face descriptor vectors have a Euclidean
-        # distance between them less than 0.6 then they are from the same
-        # person, otherwise they are from different people. Here we just print
-        # the vector to the screen.
         face_descriptor = facerec.compute_face_descriptor(img, shape)
         #print(face_descriptor)
         #print("Computing descriptor on aligned image ..")
@@ -99,10 +89,6 @@ def process_images():
 
         win.clear_overlay()
         win.set_image(img)
-
-        # Ask the detector to find the bounding boxes of each face. The 1 in the
-        # second argument indicates that we should upsample the image 1 time. This
-        # will make everything bigger and allow us to detect more faces.
         dets = detector(img, 1)
         print("Number of faces detected: {}".format(len(dets)))
 
@@ -116,38 +102,8 @@ def process_images():
             win.clear_overlay()
             win.add_overlay(d)
             win.add_overlay(shape)
-
-            # Compute the 128D vector that describes the face in img identified by
-            # shape.  In general, if two face descriptor vectors have a Euclidean
-            # distance between them less than 0.6 then they are from the same
-            # person, otherwise they are from different people. Here we just print
-            # the vector to the screen.
             face_descriptor = facerec.compute_face_descriptor(img, shape)
             print(face_descriptor)
-            # It should also be noted that you can also call this function like this:
-            #  face_descriptor = facerec.compute_face_descriptor(img, shape, 100, 0.25)
-            # The version of the call without the 100 gets 99.13% accuracy on LFW
-            # while the version with 100 gets 99.38%.  However, the 100 makes the
-            # call 100x slower to execute, so choose whatever version you like.  To
-            # explain a little, the 3rd argument tells the code how many times to
-            # jitter/resample the image.  When you set it to 100 it executes the
-            # face descriptor extraction 100 times on slightly modified versions of
-            # the face and returns the average result.  You could also pick a more
-            # middle value, such as 10, which is only 10x slower but still gets an
-            # LFW accuracy of 99.3%.
-            # 4th value (0.25) is padding around the face. If padding == 0 then the chip will
-            # be closely cropped around the face. Setting larger padding values will result a looser cropping.
-            # In particular, a padding of 0.5 would double the width of the cropped area, a value of 1.
-            # would triple it, and so forth.
-
-            # There is another overload of compute_face_descriptor that can take
-            # as an input an aligned image.
-            #
-            # Note that it is important to generate the aligned image as
-            # dlib.get_face_chip would do it i.e. the size must be 150x150,
-            # centered and scaled.
-            #
-            # Here is a sample usage of that
 
             print("Computing descriptor on aligned image ..")
 
